9b662cb0b4e90cca470756debd49969d677389f3
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2025-02-25 20:45:51 +0000

    Added FretPositions page

---
+++ Fretworks/ViewModels/FretPositionsViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Fretworks.ViewModels;
+
+public class FretPositionsViewModel
+{
+    public int Fret { get; set; }
+    public bool Done { get; set; }
+
+    // Single scale
+    public double DistFromNut { get; set; }
+    public double DistPrevFret { get; set; }
+
+    // Multi-scale
+    public double BassDistFromNut { get; set; }
+    public double BassDistPrevFret { get; set; }
+    public double TrebleDistFromNut { get; set; }
+    public double TrebleDistPrevFret { get; set; }
+}
---
+++ Fretworks/ViewModels/SelectedViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Fretworks.ViewModels;
+
+public class SelectedViewModel
+{
+    public string? Instrument { get; set; }
+    public string? MakeOrModel { get; set; }
+    public int? NumberOfFrets { get; set; }
+    public bool IsMultiScale { get; set; }
+    public bool IsSingleScale => !IsMultiScale;
+    public double? ScaleLength { get; set; }
+    public double? BassScaleLength { get; set; }
+    public double? TrebleScaleLength { get; set; }
+    public int? NeutralFret { get; set; }
+}
